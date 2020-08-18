fa0f0b45d07c46862e84ed8b6c56c2feed0741e4
LyhourChhen <ukamilove66@gmail.com>
2020-08-18 14:13:54 +0700

    init script to build in the project

---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+build() {
+    echo 'building react'
+
+    rm -rf dist/*
+
+    export INLINE_RUNTIME_CHUNK=false
+    export GENERATE_SOURCEMAP=false
+
+    npx react-scripts build
+
+    mkdir -p dist
+    cp -r build/* dist
+    rm -rf build
+    mv dist/index.html dist/index.html
+}
+
+build
---
+++ dist/precache-manifest.a0b2b2bfcf32e627840d7561ee0eedef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "964f227a463627151542586b3c626974",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d731bb724e650dcc8ea1",
+    "url": "/static/css/main.a0d7dd5f.chunk.css"
+  },
+  {
+    "revision": "9b0ec8351978b2b45974",
+    "url": "/static/js/2.658f081a.chunk.js"
+  },
+  {
+    "revision": "229313a91dfc1a4229b4758368bd8914",
+    "url": "/static/js/2.658f081a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d731bb724e650dcc8ea1",
+    "url": "/static/js/main.3ea38e60.chunk.js"
+  },
+  {
+    "revision": "bd2f60ec30321f92fa45",
+    "url": "/static/js/runtime-main.89107831.js"
+  }
+]);
